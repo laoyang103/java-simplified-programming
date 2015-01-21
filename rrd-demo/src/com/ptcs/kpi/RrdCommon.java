2a0a7bae48cae232fdd6fa7cadddba08c6ec1500
laoyang103 <553069938@qq.com>
2015-01-21 10:19:31 +0800

    60秒间隔rrd

--- rrd-demo/src/com/ptcs/kpi/RrdCommon.java
+++ rrd-demo/src/com/ptcs/kpi/RrdCommon.java
@@ -22,8 +22,9 @@ public class RrdCommon {
     private static String wholeName;
     private static String createCmd;
     private static String updateCmd;
-    private static boolean isOsLinux;
+    private static String fetchBizCmd;
     private static String fetchHostCmd;
+    private static boolean isOsLinux;
 
     static {
         try {
@@ -39,18 +40,19 @@ public class RrdCommon {
         }
 
         rrd = Rrd.getInstance();
-        wholeDir = "%OS_RRDPATH%/ipm/rrd/interfaces/device.2/hosts/%IP0%/%IP1%/%IP2%/%IP3%/%PORT%/";
-        wholeName = "%OS_RRDPATH%/ipm/rrd/interfaces/device.2/hosts/%IP0%/%IP1%/%IP2%/%IP3%/%PORT%/%KPINAME%.rrd";
-        createCmd = "create %OS_RRDPATH%/ipm/rrd/interfaces/device.2/hosts/" + 
-            "%IP0%/%IP1%/%IP2%/%IP3%/%PORT%/%KPINAME%.rrd " +
-            "--start %TIME% --step 10 DS:VALUE:GAUGE:30:U:U " + 
+        wholeDir = "%OS_RRDPATH%/ipm/rrd/app/%BIZNAME%/";
+        wholeName = "%OS_RRDPATH%/ipm/rrd/app/%BIZNAME%/%KPINAME%.rrd";
+        createCmd = "create %OS_RRDPATH%/ipm/rrd/app/%BIZNAME%/%KPINAME%.rrd --start %TIME% --step 60 " +                      
+            "DS:NORMAL:GAUGE:90:U:U DS:NORMAL_ALERT:GAUGE:90:U:U " +                                                           
+            "DS:IMPORTANT_ALERT:GAUGE:90:U:U DS:URGENT_ALERT:GAUGE:90:U:U " +
             "RRA:AVERAGE:0.5:1:144000";
-        updateCmd = "update %OS_RRDPATH%/ipm/rrd/interfaces/device.2/hosts/" + 
-            "%IP0%/%IP1%/%IP2%/%IP3%/%PORT%/%KPINAME%.rrd " +
-            "%TIME%:%VALUE%";
-        fetchHostCmd = "fetch %OS_RRDPATH%/ipm/rrd/interfaces/device.2/hosts/" + 
+        updateCmd = "update %OS_RRDPATH%/ipm/rrd/app/%BIZNAME%/%KPINAME%.rrd " + 
+            "%TIME%:%NORMAL%:%NORMAL_ALERT%:%IMPORTANT_ALERT%:%URGENT_ALERT%";
+        fetchBizCmd= "fetch %OS_RRDPATH%/ipm/rrd/app/%BIZNAME%/%KPINAME%.rrd " + 
+            "AVERAGE --start %STARTTIME% --end %ENDTIME% --resolution 60";
+        fetchHostCmd = "fetch --daemon unix:/tmp/rrdcached.sock %OS_RRDPATH%/ipm/rrd/interfaces/device.2/hosts/" + 
             "%IP0%/%IP1%/%IP2%/%IP3%/%PORT%/%KPINAME%.rrd " + 
-            "AVERAGE --start %STARTTIME% --end %ENDTIME%";// --resolution 10"; 
+            "AVERAGE --start %STARTTIME% --end %ENDTIME% --resolution 60";
 
         if (osName.equals("Linux")) {
             isOsLinux = true;
@@ -58,14 +60,16 @@ public class RrdCommon {
             wholeName = wholeName.replaceFirst("%OS_RRDPATH%", analysisPath);
             createCmd = createCmd.replaceFirst("%OS_RRDPATH%", analysisPath);
             updateCmd = updateCmd.replaceFirst("%OS_RRDPATH%", analysisPath);
+            fetchBizCmd = fetchBizCmd.replaceFirst("%OS_RRDPATH%", analysisPath);
             fetchHostCmd = fetchHostCmd.replaceFirst("%OS_RRDPATH%", analysisPath);
         } else {
             isOsLinux = false;
-            wholeDir = wholeDir.replaceFirst("%OS_RRDPATH%",   "C:/kpi/");
-            wholeName = wholeName.replaceFirst("%OS_RRDPATH%", "C:/kpi/");
-            createCmd = createCmd.replaceFirst("%OS_RRDPATH%", "C:/kpi/");
-            updateCmd = updateCmd.replaceFirst("%OS_RRDPATH%", "C:/kpi/");
-            fetchHostCmd = fetchHostCmd.replaceFirst("%OS_RRDPATH%", "C:/kpi/");
+            wholeDir = wholeDir.replaceFirst("%OS_RRDPATH%", "C:/kpi/ipm/rrd/");
+            wholeName = wholeName.replaceFirst("%OS_RRDPATH%", "C:/kpi/ipm/rrd/");
+            createCmd = createCmd.replaceFirst("%OS_RRDPATH%", "C:/kpi/ipm/rrd/");
+            updateCmd = updateCmd.replaceFirst("%OS_RRDPATH%", "C:/kpi/ipm/rrd/");
+            fetchBizCmd = fetchBizCmd.replaceFirst("%OS_RRDPATH%", "C:/kpi/ipm/rrd/");
+            fetchHostCmd = fetchHostCmd.replaceFirst("%OS_RRDPATH%", "C:/kpi/ipm/rrd/");
         }
     }
 
@@ -80,21 +84,20 @@ public class RrdCommon {
      * @param @param importantAlert-- 重要告警
      * @param @param urgentAlert-- 紧急告警
      */
-    public static void rrdWriteHostData(long time, ManagerBean mb, String kpiName, double value) {
+    public static void rrdWriteBizData(long time, String bizName, String kpiName, 
+            double normal, double normalAlert, double importantAlert, double urgentAlert) {
         try {
             String update = updateCmd;
-            String[] ips = mb.getIp().split("\\.");
-            time = Util.normalize(time, 10);
+            time = Util.normalize(time, 60);
             update = update
-                .replaceFirst("%IP0%", ips[0])
-                .replaceFirst("%IP1%", ips[1])
-                .replaceFirst("%IP2%", ips[2])
-                .replaceFirst("%IP3%", ips[3])
-                .replaceFirst("%PORT%", mb.getPort())
+                .replaceFirst("%BIZNAME%", bizName)
                 .replaceFirst("%KPINAME%", kpiName)
                 .replaceFirst("%TIME%", String.valueOf(time))
-                .replaceFirst("%VALUE%", String.valueOf(value));
-            RrdCommon.createDir(time, mb, kpiName);
+                .replaceFirst("%NORMAL%", String.valueOf(normal))
+                .replaceFirst("%NORMAL_ALERT%", String.valueOf(normalAlert))
+                .replaceFirst("%IMPORTANT_ALERT%", String.valueOf(importantAlert))
+                .replaceFirst("%URGENT_ALERT%", String.valueOf(urgentAlert));
+            RrdCommon.createDir(time, bizName, kpiName);
             System.out.println(update);
             if (isOsLinux) {
                 rrd.update(update);
@@ -104,6 +107,67 @@ public class RrdCommon {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * @Title: rrdFetchBiz
+     * @Description: rrd获取业务数据
+     * @param @param start	-- 开始时间(时间戳[s])
+     * @param @param end	-- 结束时间(时间戳[s])
+     * @param @param bizName-- 业务名称
+     * @param @param kpiName-- KPI名称
+     * @return 返回数据
+     */
+    public static List<RrdBizBean> rrdFetchBiz(long start, long end, String bizName, String kpiName) {
+        String fetch = fetchBizCmd;
+        List<RrdBizBean> fetchList = new ArrayList<RrdBizBean>();
+        try {
+            String[] setime = calcStartEndTime(start, end).split(",");
+            fetch = fetch
+                .replaceFirst("%BIZNAME%", bizName)
+                .replaceFirst("%KPINAME%", kpiName)
+                .replaceFirst("%STARTTIME%", setime[0])
+                .replaceFirst("%ENDTIME%", setime[1]);
+            System.out.println(fetch);
+            if (isOsLinux) {
+                com.ptcs.kpi.Rrd.FetchData data = (com.ptcs.kpi.Rrd.FetchData )rrd.fetch(fetch);
+                int rows = data.getRowCount();
+                long time = 0l;
+                if (rows > 0) {
+                    for (int i = 0; i < rows; i++) {
+                        time  = data.getTimestamp(i) + 60;
+                        if (start >= time) {continue;}
+                        if (end < time) {break;}
+                        RrdBizBean rbb = new RrdBizBean();
+                        rbb.setNormal((Double.isNaN(data.getValue(i, 0)) ? 0.0 : data.getValue(i, 0)));
+                        rbb.setNormalAlert((Double.isNaN(data.getValue(i, 1)) ? 0.0 : data.getValue(i, 1)));
+                        rbb.setImportantAlert((Double.isNaN(data.getValue(i, 2)) ? 0.0 : data.getValue(i, 2)));
+                        rbb.setUrgentAlert((Double.isNaN(data.getValue(i, 3)) ? 0.0 : data.getValue(i, 3)));
+                        fetchList.add(rbb);
+                    }
+                }
+            } else {
+                org.rrd4j.core.FetchData data = (org.rrd4j.core.FetchData )RrdCommander.execute(fetch);
+                int rows = data.getRowCount();
+                long time = 0l;
+                if (rows > 0) {
+                    for (int i = 0; i < rows; i++) {
+                        time  = data.getTimestamps()[i] + 60;
+                        if (start >= time) {continue;}
+                        if (end < time) {break;}
+                        RrdBizBean rbb = new RrdBizBean();
+                        rbb.setNormal((Double.isNaN(data.getValues(0)[i]) ? 0.0 : data.getValues(0)[i]));
+                        rbb.setNormalAlert((Double.isNaN(data.getValues(1)[i]) ? 0.0 : data.getValues(1)[i]));
+                        rbb.setImportantAlert((Double.isNaN(data.getValues(2)[i]) ? 0.0 : data.getValues(2)[i]));
+                        rbb.setUrgentAlert((Double.isNaN(data.getValues(3)[i]) ? 0.0 : data.getValues(3)[i]));
+                        fetchList.add(rbb);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fetchList;
     }
 
     /**
@@ -119,7 +183,7 @@ public class RrdCommon {
         String fetch = fetchHostCmd;
         List<Double> fetchList = new ArrayList<Double>();
         try {
-            String[] ips = mb.getIp().split("\\.");
+            String[] ips = mb.getIp().split(".");
             String[] setime = calcStartEndTime(start, end).split(",");
             fetch = fetch
                 .replaceFirst("%IP0%", ips[0])
@@ -130,14 +194,13 @@ public class RrdCommon {
                 .replaceFirst("%KPINAME%", kpiName)
                 .replaceFirst("%STARTTIME%", setime[0])
                 .replaceFirst("%ENDTIME%", setime[1]);
-            System.out.println(fetch);
             if (isOsLinux) {
                 com.ptcs.kpi.Rrd.FetchData data = (com.ptcs.kpi.Rrd.FetchData )rrd.fetch(fetch);
                 int rows = data.getRowCount();
                 long time = 0l;
                 if (rows > 0) {
                     for (int i = 0; i < rows; i++) {
-                        time  = data.getTimestamp(i) + 10;
+                        time  = data.getTimestamp(i) + 60;
                         if (start >= time) {continue;}
                         if (end < time) {break;}
                         fetchList.add((Double.isNaN(data.getValue(i, 0)) ? 0.0 : data.getValue(i, 0)));
@@ -149,7 +212,7 @@ public class RrdCommon {
                 long time = 0l;
                 if (rows > 0) {
                     for (int i = 0; i < rows; i++) {
-                        time  = data.getTimestamps()[i] + 10;
+                        time  = data.getTimestamps()[i] + 60;
                         if (start >= time) {continue;}
                         if (end < time) {break;}
                         fetchList.add((Double.isNaN(data.getValues(0)[i]) ? 0.0 : data.getValues(0)[i]));
@@ -162,35 +225,44 @@ public class RrdCommon {
         return fetchList;
     }
 
-    private static void createDir(long time, ManagerBean mb, String kpiName) throws Exception {
+    /**
+     * @Title: rrdFetchHostList
+     * @Description: rrd获取多台主机平均数据
+     * @param @param start	-- 开始时间(时间戳[s])
+     * @param @param end	-- 结束时间(时间戳[s])
+     * @param @param mbList -- 业务系统对象列表
+     * @param @param kpiName-- KPI名称
+     * @return 返回数据
+     */
+    public static List<Double> rrdFetchHostList(long start, long end, List<ManagerBean> mbList, String kpiName) {
+        List<Double> valList = null;
+        List<Double> retList = new ArrayList<Double>();
+        for (ManagerBean mb: mbList) {
+            valList = RrdCommon.rrdFetchHost(start, end, mb, kpiName);
+            for (int i = 0;i < valList.size();i++) {
+                retList.set(i, Double.valueOf(retList.get(i).doubleValue() + valList.get(i).doubleValue()));
+            }
+        }
+        for (int i = 0;i < retList.size();i++) {
+            retList.set(i, Double.valueOf(retList.get(i).doubleValue() / mbList.size()));
+        }
+        return retList;
+    }
+
+    private static void createDir(long time, String bizName, String kpiName) throws Exception {
         String create = createCmd, whole = wholeDir;
-        String[] ips = mb.getIp().split("\\.");
-        whole = whole
-            .replaceFirst("%IP0%", ips[0])
-            .replaceFirst("%IP1%", ips[1])
-            .replaceFirst("%IP2%", ips[2])
-            .replaceFirst("%IP3%", ips[3])
-            .replaceFirst("%PORT%", mb.getPort());
+        whole = whole.replaceFirst("%BIZNAME%", bizName);
         File file = new File(whole);
         file.mkdirs();
         whole = wholeName;
-        whole = whole
-            .replaceFirst("%IP0%", ips[0])
-            .replaceFirst("%IP1%", ips[1])
-            .replaceFirst("%IP2%", ips[2])
-            .replaceFirst("%IP3%", ips[3])
-            .replaceFirst("%PORT%", mb.getPort())
-            .replaceFirst("%KPINAME%", kpiName);
+        whole = whole.replaceFirst("%BIZNAME%", bizName).replaceFirst("%KPINAME%", kpiName);
         file = new File(whole);
         if (!file.exists()) {
             create = create
-                .replaceFirst("%IP0%", ips[0])
-                .replaceFirst("%IP1%", ips[1])
-                .replaceFirst("%IP2%", ips[2])
-                .replaceFirst("%IP3%", ips[3])
-                .replaceFirst("%PORT%", mb.getPort())
-                .replaceFirst("%KPINAME%", kpiName)
+                .replace("%BIZNAME%", bizName)
+                .replace("%KPINAME%", kpiName)
                 .replace("%TIME%", String.valueOf(time-1));
+            System.out.println(create);
             if (isOsLinux) {
                 rrd.create(create);
             } else {
@@ -226,19 +298,13 @@ public class RrdCommon {
     }
 
     public static void main(String[] args) {
-        System.out.println("=================");
-        ManagerBean mb = new ManagerBean();
-        mb.setPort("21");
-        mb.setIp("61.138.125.44");
-        double a = 1.5;
-
         while (true) {
             try {
-                long now = System.currentTimeMillis() / 1000;
-                rrdWriteHostData(now, mb, "test", a);
-                System.out.println(rrdFetchHost(now - 60, now, mb, "test"));
-                Thread.sleep(11000);
-                a += 1;
+                long now = System.currentTimeMillis() / 1000;                                                                      
+                RrdCommon.rrdWriteBizData(now, "a", "test", 1.4f, 2.5f, 3.6f, 4.7f);                                               
+                List<RrdBizBean> data = RrdCommon.rrdFetchBiz(now - 600, now, "a", "test");
+                System.out.println(data);
+                Thread.sleep(60000);
             } catch (Exception e) {
                 e.printStackTrace(); 
             }
